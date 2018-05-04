3feac136356e4b8bb7387e1b94d247c53dabb161
Fancy-That <38474320+Fancy-That@users.noreply.github.com>
2018-05-04 13:48:52 +0100

    Update primeFactorisation.js

--- primeFactorisation.js
+++ primeFactorisation.js
@@ -1,1 +1,1 @@
-let g=(a,b)=>a%b?g(b,a%b):b,primeFactor=(_,$=[],x,i=_)=>{for(;--i;)x=g(_,i),x>1?($.push(x),x=pFactor(x)[0],_/=x,i=_):0;return[_,$]};
+let g=(a,b)=>a%b?g(b,a%b):b,pFactor=(_,$=[],x,i=_)=>{for(;--i;)x=g(_,i),x>1?($.push(x),x=pFactor(x)[0],_/=x,i=_):0;return[_,...$]};
